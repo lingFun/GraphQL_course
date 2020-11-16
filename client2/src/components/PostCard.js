7e0379092915f29d8b5d378cd200ffeaa215cd25
lingFun <49457941+lingFun@users.noreply.github.com>
2020-11-16 14:41:35 -0500

    class10 info popup

--- client2/src/components/PostCard.js
+++ client2/src/components/PostCard.js
@@ -6,6 +6,8 @@ import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
+
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes }
@@ -28,6 +30,8 @@ function PostCard({
       </Card.Content>
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <MyPopup content="Comment on post">
+
         <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
           <Button color="blue" basic>
             <Icon name="comments" />
@@ -36,6 +40,7 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
+        </MyPopup>
         {/* {user && user.username === username && (
           <Button
             as="div"
